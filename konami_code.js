32bc2e1ef92b25a3c536d2f5ef614682079d9be8
Eric C <erix2205@gmail.com>
2020-07-22 08:56:28 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,20 +15,16 @@ function init() {
   var count = 0;
 
   function keyHandler(){
-    for (let i = 0 ; i <= codes.length ; i++){
-
-      if ('keydown' === codes[i]){
-        count++;
-      }
-      else if (count === codes.length) {
+    if ('keydown' === codes[i]){
+      count++;
+      if (count === codes.length) {
         alert("Congratulations! You've hacked it!");
         count = 0;
-      }
-      else {
-        count = 0;
-      }
-    }
-  }
+    }}
+    else {
+      count = 0
+    }}
+
 
   document.body.addEventListener("keydown", keyHandler())
 }
